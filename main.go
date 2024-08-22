468fc99a6b59176f8d0f339145ffc58cc93f1af6
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-08-22 22:00:18 +0200

    Setup serial port

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/tarm/serial"
+	"log"
+)
+
+func main() {
+	config := &serial.Config{
+		Name: "COM3",
+		Baud: 115200, // I hope that higher baud rate will reduce input lag
+	}
+
+	port, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(port *serial.Port) {
+		err := port.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(port)
+}
